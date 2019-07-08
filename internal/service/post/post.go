c35f19fa3f05e2b5f4a90842a775638f58cfb623
natsuki <yukihiratype2@live.com>
2019-07-08 16:33:33 +0800

    commit message

--- internal/service/post/post.go
+++ internal/service/post/post.go
@@ -3,9 +3,7 @@ package postservice
 import (
 	"github.com/yukihiratype2/cookbook-server/internal/db"
 	m "github.com/yukihiratype2/cookbook-server/internal/model/app"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"strconv"
 )
 
 type PostService struct {
@@ -28,16 +26,4 @@ func (ps *PostService) GetPostByID(postID string) (findedPost *m.Post, err error
 		ID: postIDInObject,
 	}
 	return ps.PostDB.Get(&postToFind)
-}
-
-//RatePost accept a int
-func (ps *PostService) RatePost(postID primitive.ObjectID, rate int) (rateAfterVote m.PostRate, err error) {
-	err = ps.PostDB.Update(postID, &bson.D{{"$inc", bson.D{
-		{"rate." + strconv.Itoa(rate), 1},
-	}}})
-	if err != nil {
-		return
-	}
-
-	return
 }
